6bc0e286e797698ae6971df5df71a0f40f0533c3
tobb <topipaavilainen@gmail.com>
2013-09-22 16:05:47 +0300

    jenkinsin virittelyä

--- src/test/java/boileri/viitehallintajarjestelma/ViitehallintajarjestelmaTest.java
+++ src/test/java/boileri/viitehallintajarjestelma/ViitehallintajarjestelmaTest.java
@@ -1,19 +1,12 @@
 package boileri.viitehallintajarjestelma;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import boileri.viitehallintajarjestelma.Viitehallintajarjestelma;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author topi
- */
+
 public class ViitehallintajarjestelmaTest {
     
     public ViitehallintajarjestelmaTest() {
@@ -27,9 +20,5 @@ public class ViitehallintajarjestelmaTest {
     public void testimetodiJenkinsille(){
         assertEquals(9, Viitehallintajarjestelma.testiMetodi(7));
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
 }
