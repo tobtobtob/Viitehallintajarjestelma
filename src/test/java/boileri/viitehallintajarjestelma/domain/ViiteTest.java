7dfa5b8b58eef42e1e0e382d86ed3ccce53ef278
Otto Hinkkanen <otto.hinkkanen@helsinki.fi>
2013-09-26 23:52:23 +0300

    Viitetestit tehty ainakin line coveragemielessa...

---
+++ src/test/java/boileri/viitehallintajarjestelma/domain/ViiteTest.java
@@ -0,0 +1,98 @@
+package boileri.viitehallintajarjestelma.domain;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Outt
+ */
+public class ViiteTest {
+
+    public ViiteTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void getPakollisetTest() {
+        List<String> actual = Viite.getPakollisetKentat("inproceedings");
+        List<String> expected = new ArrayList<String>() {
+        };
+        expected.add("Author: ");
+        expected.add("Title: ");
+        expected.add("Book title: ");
+        expected.add("Year: ");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void luoViiteTest() {
+
+        List<String> otsikot = new ArrayList<String>();
+        otsikot.add("A");
+        otsikot.add("T");
+        otsikot.add("B");
+        otsikot.add("Y");
+        Viite x = Viite.luoViite(otsikot, "inproceedings");
+        String expected = "Author: A\nTitle: T\nBook title: B\nYear: Y\n";
+        assertEquals(x.toString(), expected);
+    }
+
+    @Test
+    public void luoViiteNullTest() {
+        List<String> otsikot = new ArrayList<String>();
+        otsikot.add("A");
+        otsikot.add("T");
+        otsikot.add("B");
+        otsikot.add("Y");
+        Viite x = Viite.luoViite(otsikot, "porkkana");
+        assertNull(x);
+    }
+
+    @Test
+    public void luoViiteNullTest2() {
+        List<String> otsikot = new ArrayList<String>();
+        otsikot.add("A");
+        otsikot.add("T");
+        otsikot.add("");
+        otsikot.add("Y");
+        Viite x = Viite.luoViite(otsikot, "inproceedings");
+        assertNull(x);
+    }
+
+    @Test
+    public void getPakollisetNullTest() {
+        List<String> actual = Viite.getPakollisetKentat("kurpitsa");
+        List<String> expected = null;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void toStringTest() {
+    }
+}
