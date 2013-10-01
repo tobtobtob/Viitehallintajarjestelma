bf4ab0b037795649a819a27afbac91793572e7a4
tobb <topipaavilainen@gmail.com>
2013-10-01 23:11:23 +0300

    pari testiä bibtexille lisätty

--- src/test/java/BibTexKirjoittajaTest.java
+++ src/test/java/BibTexKirjoittajaTest.java
@@ -7,6 +7,7 @@ import boileri.viitehallintajarjestelma.BibTexKirjoittaja;
 import boileri.viitehallintajarjestelma.Viitehallintajarjestelma;
 import boileri.viitehallintajarjestelma.domain.Viite;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
@@ -18,7 +19,7 @@ public class BibTexKirjoittajaTest {
     
     FileWriter fw;
     BibTexKirjoittaja gen;
-    Viite viite;
+    Viite viite, viite2;
     
     public BibTexKirjoittajaTest() {
     }
@@ -27,14 +28,27 @@ public class BibTexKirjoittajaTest {
     public void setUp() {
         fw = mock(FileWriter.class);
         viite = mock(Viite.class);
+        viite2 = mock(Viite.class);
         gen = new BibTexKirjoittaja(fw);
     }
     @Test
-    public void kirjoittaaYhdenViitteenOikein(){
+    public void kirjoittaaYhdenViitteenOikein() throws IOException{
         List<Viite> viitteet = new ArrayList<Viite>();
         viitteet.add(viite);
-        when(viite.toBibTex()).thenReturn("@testiviite{839\nauthor = {testimies},\n}");
+        when(viite.toBibTex()).thenReturn("@testiviite{839\nauthor = {testimies},\n}\n");
         gen.writeBibTex(viitteet, "testi");
+        verify(fw).write("@testiviite{839\nauthor = {testimies},\n}\n");
+    }
+    @Test
+    public void kirjoittaaUseammanViitteenOikein() throws IOException{
+        List<Viite> viitteet = new ArrayList<Viite>();
+        viitteet.add(viite);
+        viitteet.add(viite2);
+        when(viite.toBibTex()).thenReturn("@testiviite{839\nauthor = {testimies},\n}\n");
+        when(viite2.toBibTex()).thenReturn("@toinen{879\nauthor = {testimies},\n}\n");
+        gen.writeBibTex(viitteet, "nakki");
+        verify(fw).write("@testiviite{839\nauthor = {testimies},\n}\n");
+        verify(fw).write("@toinen{879\nauthor = {testimies},\n}\n");
     }
     
 }
